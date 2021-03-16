43aa471a7ea65fd9a0b797899164c134f4448c08
Oleg <sosolala404@gmail.com>
2021-03-16 17:06:18 +0300

    newCommit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,12 +10,12 @@ import ru.netology.sender.MessageSenderImpl;
 
 public class Main {
 
-    //Тестовый пример
-    public static void main(String[] args) {
+    //Тестовый пример only my
+    public static void main(String[] args ) {
         GeoService geoService = new GeoServiceImpl();
         LocalizationService localizationService = new LocalizationServiceImpl();
         MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
-
+//new commit
         Map<String, String> headers = new HashMap<String, String>();
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
         messageSender.send(headers);
